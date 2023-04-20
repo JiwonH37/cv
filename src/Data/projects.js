cc278a46f8882aeca62e9e18160e2112dcc2d0ed
JiwonH37 <hongjiwon@Jiwons-MBP.attlocal.net>
2023-04-20 07:21:22 -0700

    Add images.

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -28,7 +28,7 @@ export const projects = [
     name: 'chipotle',
     overview:
       'This project is an online ordering system for Chipotle. The users are able to type their name, click ingredients, and get the total price. It is created using Python. ',
-    link: 'https://github.com',
+    link: 'https://codehs.com/sandbox/id/new-sandbox-program-DjFpjV/run',
     images: [
       '/Images/chipotle/1.png',
       '/Images/chipotle/2.png',
